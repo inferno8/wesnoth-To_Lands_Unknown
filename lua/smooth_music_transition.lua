d01d80eea68d4796b78bf681c5ce0a4b5afe9781
inferno8 <inferno-8@wp.pl>
2022-01-01 21:38:14 +0100

    remove deprecated code

--- lua/smooth_music_transition.lua
+++ lua/smooth_music_transition.lua
@@ -1,7 +1,7 @@
 --music code written by ForestDragon:
     function TLU_smooth_replace_music(new_track,fade_out_time,fade_in_time)
     
-        wesnoth.music_list.volume = 100 --percentage of the volume in settings
+        wesnoth.audio.music_list.volume = 100 --percentage of the volume in settings
     
         if fade_out_time then else
             fade_out_time = 2000-- if no delay is provided in the function, use a default value
@@ -9,17 +9,17 @@
     
         delay_time = fade_out_time / 10 -- since the fade out takes roughly 10 steps, divide the time by 10
     
-        while wesnoth.music_list.volume >= 10 do
-            wesnoth.music_list.volume = wesnoth.music_list.volume - 10
+        while wesnoth.audio.music_list.volume >= 10 do
+            wesnoth.audio.music_list.volume = wesnoth.audio.music_list.volume - 10
 --            repeats = repeats - 1
-            wesnoth.delay(delay_time)
---            wesnoth.message(wesnoth.music_list.volume)
+            wesnoth.interface.delay(delay_time)
+--            wesnoth.message(wesnoth.audio.music_list.volume)
         end
 
-        wesnoth.music_list.volume = 0
+        wesnoth.audio.music_list.volume = 0
     
-        wesnoth.music_list.clear()
-        wesnoth.music_list.add(new_track, true)
+        wesnoth.audio.music_list.clear()
+        wesnoth.audio.music_list.add(new_track, true)
 
         if fade_in_time then else
             fade_in_time = 0-- if no delay is provided in the function, fade in is instant
@@ -27,13 +27,13 @@
     
         delay_time = fade_in_time / 10 -- since the fade out takes roughly 10 steps, divide the time by 10
 
-        while wesnoth.music_list.volume <= 90 do
-            wesnoth.music_list.volume = wesnoth.music_list.volume + 10
+        while wesnoth.audio.music_list.volume <= 90 do
+            wesnoth.audio.music_list.volume = wesnoth.audio.music_list.volume + 10
 --            repeats = repeats - 1
-            wesnoth.delay(delay_time)
---            wesnoth.message(wesnoth.music_list.volume)
+            wesnoth.interface.delay(delay_time)
+--            wesnoth.message(wesnoth.audio.music_list.volume)
         end
 
-        wesnoth.music_list.volume = 100
+        wesnoth.audio.music_list.volume = 100
     
     end
