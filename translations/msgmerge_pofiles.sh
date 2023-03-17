c83aa43cdf5dd067db3a9fcc9302d76f16885831
inferno8 <inferno-8@wp.pl>
2023-03-17 20:25:33 +0100

    update msgmerge_pofiles.sh

--- translations/msgmerge_pofiles.sh
+++ translations/msgmerge_pofiles.sh
@@ -4,6 +4,6 @@
 # Then, you can run this script
 
 for mylang in de es fr hu it pl ru zh_CN; do
-	msgmerge --previous --update --lang=${mylang} wesnoth-To_Lands_Unknown/${mylang}.po TLU_translation_template.pot;
-	msgfmt -o wesnoth-To_Lands_Unknown/${mylang}.mo wesnoth-To_Lands_Unknown/${mylang}.po;
+	msgmerge --previous --update --lang=${mylang} ${mylang}/LC_MESSAGES/wesnoth-To_Lands_Unknown.po TLU_translation_template.pot;
+	msgfmt -o ${mylang}/LC_MESSAGES/wesnoth-To_Lands_Unknown.mo ${mylang}/LC_MESSAGES/wesnoth-To_Lands_Unknown.po;
 done
